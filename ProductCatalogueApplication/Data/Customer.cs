8e8d53d61985558ad7c98c511e7acf99ebec57e0
Ingrid <ingrid97.skoglund@gmail.com>
2021-10-19 11:10:59 +0200

    Customer ändring
    Customer ändring

--- ProductCatalogueApplication/Data/Customer.cs
+++ ProductCatalogueApplication/Data/Customer.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 namespace ProductCatalogueApplication.Data
 {
     public class Customer
     {
-
+        //+Id : int
+        public int Id;
+        //+Name : string
+        public string Name;
+        //+Phone : string
+        public string Phone;
+        //+Email : string
+        public string Email;
+        //+Orders : List<Order>
+        List<Order> orders = new List<Order>();
     }
 }
