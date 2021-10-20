83b6ed6fafd2615c99f31ac5ed0b88b30b187a4a
Ingrid <ingrid97.skoglund@gmail.com>
2021-10-20 15:26:24 +0200

    hej
    hej

--- ProductCatalogueApplication/Data/Interfaces/ICustomerRepository.cs
+++ ProductCatalogueApplication/Data/Interfaces/ICustomerRepository.cs
@@ -7,5 +7,10 @@ namespace ProductCatalogueApplication.Data.Interfaces
 {
     interface ICustomerRepository
     {
+        void AddCustomer(Customer customer);
+        void RemoveCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        public Task<List<Customer>> DisplayArchivedCustomerOrder(Order order);
+        public Task<List<Customer>> DisplayActiveCustomerOrder(Order order);
     }
 }
--- ProductCatalogueApplication/Data/Repositories/CustomerRepository.cs
+++ ProductCatalogueApplication/Data/Repositories/CustomerRepository.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ProductCatalogueApplication.Data.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -17,10 +18,46 @@ namespace ProductCatalogueApplication.Data
             _context = context;
         }
 
-        //Här kan vi lägga in metoder som rör Customer, bara för start en metod för att hämta alla customers som finns lagrade 
+        //H?r kan vi l?gga in metoder som r?r Customer, bara f?r start en metod f?r att h?mta alla customers som finns lagrade 
         public async Task<List<Customer>> GetCustomersAsync()
         {
             return await _context.Customers.ToListAsync();
         }
+
+        //Customer Add metod
+        public void AddCustomer(Customer customer)
+        {
+            _context.Add(customer);
+        }
+
+        public void RemoveCustomer(Customer customer)
+        {
+            _context.Remove(customer);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            _context.Update(customer);
+        }
+
+        public async Task<List<Customer>> DisplayArchivedCustomerOrder(Order order)
+        {
+            Task<List<Customer>> CustomerList =_context.Customers.ToListAsync();
+
+            CustomerList.Result
+
+                //where ...bool is true??
+
+            return await ...;
+        }
+
+        public async Task<List<Customer>> DisplayActiveCustomerOrder(Order order)
+        {
+            //where ...bool is true??
+            return await ...;
+        }
+
+
+
     }
 }
