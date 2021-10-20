b349e3b4461290b8daaa77f1d34ef091a3a90a32
Alexander Arvidsson <to.alexander.a@gmail.com>
2021-10-20 16:40:03 +0200

    Fix
    Fixes

--- ProductCatalogueApplication/Data/Interfaces/ICustomerRepository.cs
+++ ProductCatalogueApplication/Data/Interfaces/ICustomerRepository.cs
@@ -7,5 +7,12 @@ namespace ProductCatalogueApplication.Data.Interfaces
 {
     interface ICustomerRepository
     {
+
+        void AddCustomer(Customer customer);
+        void RemoveCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        public Task<List<Order>> DisplayArchivedCustomerOrder(Customer customer);
+        public Task<List<Order>> DisplayActiveCustomerOrder(Customer customer);
+        public Task<List<Customer>> GetCustomersAsync();
     }
 }
--- ProductCatalogueApplication/Data/Repositories/CustomerRepository.cs
+++ ProductCatalogueApplication/Data/Repositories/CustomerRepository.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using ProductCatalogueApplication.Data.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProductCatalogueApplication.Data
@@ -17,10 +19,39 @@ namespace ProductCatalogueApplication.Data
             _context = context;
         }
 
-        //Här kan vi lägga in metoder som rör Customer, bara för start en metod för att hämta alla customers som finns lagrade 
+        //H?r kan vi l?gga in metoder som r?r Customer, bara f?r start en metod f?r att h?mta alla customers som finns lagrade 
         public async Task<List<Customer>> GetCustomersAsync()
         {
             return await _context.Customers.ToListAsync();
         }
+
+        public void AddCustomer(Customer customer)
+        {
+            _context.Add(customer);
+        }
+
+        public void RemoveCustomer(Customer customer)
+        {
+            _context.Remove(customer);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            _context.Update(customer);
+        }
+
+        public async Task<List<Order>> DisplayArchivedCustomerOrder(Customer customer)
+        {
+
+            return await _context.Orders.Where(o => o.Dispatched == true && customer.Id == o.CustomerId).ToListAsync();
+            
+        }
+
+        public async Task<List<Order>> DisplayActiveCustomerOrder(Customer customer)
+        {
+            
+            return await _context.Orders.Where(o => o.Dispatched == false && customer.Id == o.CustomerId).ToListAsync();
+        }
+
     }
 }
